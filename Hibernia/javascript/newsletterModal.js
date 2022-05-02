3dfc4f512033816ac2112f8edc7d77d7f0f7f053
DarrenRobertLowe <61787379+DarrenRobertLowe@users.noreply.github.com>
2022-05-02 16:13:41 +0100

    Created a popup modal for index.html
    The user is now prompted to enter their email to register for the newsletter.

---
+++ Hibernia/javascript/newsletterModal.js
@@ -0,0 +1,41 @@
+// not sure of law surrounding non-tracking cookies
+/* localStorage.setItem('IsModalShown', false); */ 
+
+
+// Get the modal
+var modal = document.getElementById("newsletterModal");
+
+// Get the button that opens the modal
+/* var btn = document.getElementById("myBtn"); */
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+
+
+$(document).ready(function(){
+   setTimeout(function(){
+       modal.style.display = "block";
+   }, 2000);
+});
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
+
+
+
+function submitEmail(){
+	/* hide the modal now */
+	modal.style.display = "none";
+}
