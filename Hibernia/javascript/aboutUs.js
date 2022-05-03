06838f170cca9be0526c2595e29d9d81df3b098a
MayRegio <104643020+MayRegio@users.noreply.github.com>
2022-05-04 00:01:13 +0100

    submit button - modal

--- Hibernia/javascript/aboutUs.js
+++ Hibernia/javascript/aboutUs.js
@@ -31,12 +31,12 @@ $(document).ready(function() {
 		$('.emailError').hide();
 	  }
 
-    if(comments == ""){
+    /*if(comments == ""){
       $('.commentsError').show();
         return false;
       } else {
       $('.commentsError').hide();
-    }
+    }*/
 
     $.post("", $("#contactForm").serialize(),  function(response) {
       $('#contactForm').fadeOut('slow',function(){
@@ -57,3 +57,28 @@ return true;
 }
 }
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var submit = document.getElementById("Send");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+submit.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
