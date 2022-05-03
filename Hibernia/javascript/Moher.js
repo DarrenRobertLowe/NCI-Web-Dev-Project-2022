617bd0be1d46f9e707c467120142f753e942f1e5
mollyshao <huixuanshao@gmail.com>
2022-05-03 15:10:50 +0100

    make one file for css and js for destiantion

--- Hibernia/javascript/Moher.js
+++
@@ -1,9 +0,0 @@
-
-var choices = ["", "Sunday June 26, 2022 (3 days / 2 nights)", "Sunday August 21, 2022 (3 days / 2 nights)"];
-
-window.onload = function() {
-  document.getElementById("date").onchange=function() {
-    document.getElementById("datemessage").innerHTML=choices[this.value];
-  }
-  document.getElementById("date").onchange(); // show default
-}
