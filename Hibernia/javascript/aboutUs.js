8d67196ad5b1af52723257ecd95095ed1e77dcfe
DarrenRobertLowe <61787379+DarrenRobertLowe@users.noreply.github.com>
2022-05-04 14:23:53 +0100

    Fixing comments validation

--- Hibernia/javascript/aboutUs.js
+++ Hibernia/javascript/aboutUs.js
@@ -2,6 +2,39 @@ function confirm_reset(){
   return confirm("Please confirm you want to reset the page");
 }
 
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var submit = document.getElementById("Send");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+/*
+submit.onclick = function() {
+  modal.style.display = "block";
+}
+*/
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
 $(document).ready(function() {
   $('.nameError').hide();
   $('.emailError').hide();
@@ -11,6 +44,7 @@ $(document).ready(function() {
     var name = $('#name').val();
     var email = $('#email').val();
     var comments = $('#comments').val();
+	
 	
     if(name == ""){
 		$('.nameError').show();
@@ -31,13 +65,18 @@ $(document).ready(function() {
 		$('.emailError').hide();
 	  }
 
-    /*if(comments == ""){
-      $('.commentsError').show();
-        return false;
-      } else {
-      $('.commentsError').hide();
-    }*/
 
+
+	if(document.getElementById("comments").value.trim() == ''){
+		$('.commentsError').show();
+		return false;
+	} else {
+		$('.commentsError').hide();
+		modal.style.display = "block";	// show the modal
+		$('#contactForm').trigger("reset");	// reset the form
+	}
+	
+	
     $.post("", $("#contactForm").serialize(),  function(response) {
       $('#contactForm').fadeOut('slow',function(){
       $('#correct').html(response);
@@ -57,28 +96,4 @@ return true;
 }
 }
 
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var submit = document.getElementById("Send");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-submit.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
